869815f480e58e2dde5e85804199f5690c10f710
yuzumikan3-kan <2201115059xk@cyber-u.ac.jp>
2025-07-31 09:13:08 +0900

    add env

---
+++ routes/notes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); //corsミドルウェアを追加
+require('dotenv').config();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
+})
+
+module.exports = router;
